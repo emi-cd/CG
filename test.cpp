4f279fa9e87b3309f612995d1c68e44630ad8c96
Emi <emi02cd@gmail.com>
2017-11-24 20:54:05 +0900

    modify test.cpp

--- test.cpp
+++ test.cpp
@@ -1,205 +1,421 @@
+
+/* Copyright (c) Mark J. Kilgard, 1994. */
+
+/**
+ * (c) Copyright 1993, Silicon Graphics, Inc.
+ * ALL RIGHTS RESERVED 
+ * Permission to use, copy, modify, and distribute this software for 
+ * any purpose and without fee is hereby granted, provided that the above
+ * copyright notice appear in all copies and that both the copyright notice
+ * and this permission notice appear in supporting documentation, and that 
+ * the name of Silicon Graphics, Inc. not be used in advertising
+ * or publicity pertaining to distribution of the software without specific,
+ * written prior permission. 
+ *
+ * THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS"
+ * AND WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY OR
+ * FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL SILICON
+ * GRAPHICS, INC.  BE LIABLE TO YOU OR ANYONE ELSE FOR ANY DIRECT,
+ * SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY
+ * KIND, OR ANY DAMAGES WHATSOEVER, INCLUDING WITHOUT LIMITATION,
+ * LOSS OF PROFIT, LOSS OF USE, SAVINGS OR REVENUE, OR THE CLAIMS OF
+ * THIRD PARTIES, WHETHER OR NOT SILICON GRAPHICS, INC.  HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
+ * POSSESSION, USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * US Government Users Restricted Rights 
+ * Use, duplication, or disclosure by the Government is subject to
+ * restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
+ * (c)(1)(ii) of the Rights in Technical Data and Computer Software
+ * clause at DFARS 252.227-7013 and/or in similar or successor
+ * clauses in the FAR or the DOD or NASA FAR Supplement.
+ * Unpublished-- rights reserved under the copyright laws of the
+ * United States.  Contractor/manufacturer is Silicon Graphics,
+ * Inc., 2011 N.  Shoreline Blvd., Mountain View, CA 94039-7311.
+ *
+ * OpenGL(TM) is a trademark of Silicon Graphics, Inc.
+ */
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
 #include <GLUT/glut.h>
 
-// https://tokoik.github.io/opengl/libglut.html#4.1 から練習用に作成したプログラム
+#define MAXOBJS 10000
+#define MAXSELECT 100
+#define MAXFEED 300
+#define SOLID 1
+#define LINE 2
+#define POINT 3
 
-GLdouble vertex[][3] = {
-  { 0.0, 0.0, 0.0 },
-  { 1.0, 0.0, 0.0 },
-  { 1.0, 1.0, 0.0 },
-  { 0.0, 1.0, 0.0 },
-  { 0.0, 0.0, 1.0 },
-  { 1.0, 0.0, 1.0 },
-  { 1.0, 1.0, 1.0 },
-  { 0.0, 1.0, 1.0 }
-};
+GLint windW = 300, windH = 300;
 
-int edge[][2] = {
-  { 0, 1 },
-  { 1, 2 },
-  { 2, 3 },
-  { 3, 0 },
-  { 4, 5 },
-  { 5, 6 },
-  { 6, 7 },
-  { 7, 4 },
-  { 0, 4 },
-  { 1, 5 },
-  { 2, 6 },
-  { 3, 7 }
-};
+GLuint selectBuf[MAXSELECT];
+GLfloat feedBuf[MAXFEED];
+GLint vp[4];
+float zRotation = 90.0;
+float zoom = 1.0;
+GLint objectCount;
+GLint numObjects;
+struct object {
+  float v1[2];
+  float v2[2];
+  float v3[2];
+  float color[3];
+} objects[MAXOBJS];
+GLenum linePoly = GL_FALSE;
 
-int face[][4] = {
-  { 0, 1, 2, 3 }, /* A-B-C-D を結ぶ面 */
-  { 1, 5, 6, 2 }, /* B-F-G-C を結ぶ面 */
-  { 5, 4, 7, 6 }, /* F-E-H-G を結ぶ面 */
-  { 4, 0, 3, 7 }, /* E-A-D-H を結ぶ面 */
-  { 4, 5, 1, 0 }, /* E-F-B-A を結ぶ面 */
-  { 3, 2, 6, 7 }  /* D-C-G-H を結ぶ面 */
-};
-
-GLdouble color[][3] = {
-  { 1.0, 0.0, 0.0 }, /* 赤 */
-  { 0.0, 1.0, 0.0 }, /* 緑 */
-  { 0.0, 0.0, 1.0 }, /* 青 */
-  { 1.0, 1.0, 0.0 }, /* 黄 */
-  { 1.0, 0.0, 1.0 }, /* マゼンタ */
-  { 0.0, 1.0, 1.0 }  /* シアン 　*/
-};
-
-GLdouble normal[][3] = {
-  { 0.0, 0.0,-1.0 },
-  { 1.0, 0.0, 0.0 },
-  { 0.0, 0.0, 1.0 },
-  {-1.0, 0.0, 0.0 },
-  { 0.0,-1.0, 0.0 },
-  { 0.0, 1.0, 0.0 }
-};
-
-GLfloat light0pos[] = { 0.0, 3.0, 5.0, 1.0 };
-GLfloat light1pos[] = { 5.0, 3.0, 0.0, 1.0 };
-
-GLfloat green[] = { 0.0, 1.0, 0.0, 1.0 };
-GLfloat red[] = { 0.8, 0.2, 0.2, 1.0 };
-GLfloat blue[] = { 0.2, 0.2, 0.8, 1.0 };
-
-void cube(void)
+static void
+InitObjects(GLint num)
 {
-  int i;
-  int j;
+  GLint i;
+  float x, y;
 
-  glBegin(GL_QUADS);
-  for (j = 0; j < 6; ++j) {
-    glNormal3dv(normal[j]);
-    for (i = 0; i < 4; ++i) {
-      glVertex3dv(vertex[face[j][i]]);
-    }
+  if (num > MAXOBJS) {
+    num = MAXOBJS;
   }
-  glEnd();
+  if (num < 1) {
+    num = 1;
+  }
+  objectCount = num;
+
+  srand((unsigned int) time(NULL));
+  for (i = 0; i < num; i++) {
+    x = (rand() % 300) - 150;
+    y = (rand() % 300) - 150;
+
+    objects[i].v1[0] = x + (rand() % 50) - 25;
+    objects[i].v2[0] = x + (rand() % 50) - 25;
+    objects[i].v3[0] = x + (rand() % 50) - 25;
+    objects[i].v1[1] = y + (rand() % 50) - 25;
+    objects[i].v2[1] = y + (rand() % 50) - 25;
+    objects[i].v3[1] = y + (rand() % 50) - 25;
+    objects[i].color[0] = ((rand() % 100) + 50) / 150.0;
+    objects[i].color[1] = ((rand() % 100) + 50) / 150.0;
+    objects[i].color[2] = ((rand() % 100) + 50) / 150.0;
+  }
 }
 
-
-void idle(void)
+static void
+Init(void)
 {
-  glutPostRedisplay();
+  numObjects = 10;
+  InitObjects(numObjects);
 }
 
-void display(void)
+static void
+Reshape(int width, int height)
 {
-  static int r = 0; /* 回転角 */
+  windW = width;
+  windH = height;
+  glViewport(0, 0, windW, windH);
+  glGetIntegerv(GL_VIEWPORT, vp);
+}
 
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+static void
+Render(GLenum mode)
+{
+  GLint i;
 
-  /* 光源の位置設定 */
-  glLightfv(GL_LIGHT0, GL_POSITION, light0pos);
-  glLightfv(GL_LIGHT1, GL_POSITION, light1pos);
+  for (i = 0; i < objectCount; i++) {
+    if (mode == GL_SELECT) {
+      glLoadName(i);
+    }
+    glColor3fv(objects[i].color);
+    glBegin(GL_POLYGON);
+    glVertex2fv(objects[i].v1);
+    glVertex2fv(objects[i].v2);
+    glVertex2fv(objects[i].v3);
+    glEnd();
+  }
+}
 
-  /* モデルビュー変換行列の保存 */
+static GLint
+DoSelect(GLint x, GLint y)
+{
+  GLint hits;
+
+  glSelectBuffer(MAXSELECT, selectBuf);
+  glRenderMode(GL_SELECT);
+  glInitNames();
+  glPushName(~0);
+
   glPushMatrix();
 
-  /* 図形の回転 */
-  glRotated((double)r, 0.0, 1.0, 0.0);
-
-  /* 図形の色 (赤)  */
-  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, red);
-
-  /* 図形の描画 */
-  cube();
-
-  /* 二つ目の図形の描画 */
-  glPushMatrix();
-  glTranslated(1.0, 1.0, 1.0);
-  glRotated((double)(2 * r), 0.0, 1.0, 0.0);
-  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, blue);
-  cube();
-  glPopMatrix();
-
-  /* モデルビュー変換行列の復帰 */
-  glPopMatrix();
-
-  glutSwapBuffers();
-
-  /* 一周回ったら回転角を 0 に戻す */
-  if (++r >= 360) r = 0;
-}
-
-void resize(int w, int h)
-{
-  glViewport(0, 0, w, h);
-
-  /* 透視変換行列の設定 */
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
-
-  /* モデルビュー変換行列の設定 */
+  gluPickMatrix(x, windH - y, 4, 4, vp);
+  gluOrtho2D(-175, 175, -175, 175);
   glMatrixMode(GL_MODELVIEW);
+
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glScalef(zoom, zoom, zoom);
+  glRotatef(zRotation, 0, 0, 1);
+
+  Render(GL_SELECT);
+
+  glPopMatrix();
+
+  hits = glRenderMode(GL_RENDER);
+  if (hits <= 0) {
+    return -1;
+  }
+  return selectBuf[(hits - 1) * 4 + 3];
+}
+
+static void
+RecolorTri(GLint h)
+{
+  objects[h].color[0] = ((rand() % 100) + 50) / 150.0;
+  objects[h].color[1] = ((rand() % 100) + 50) / 150.0;
+  objects[h].color[2] = ((rand() % 100) + 50) / 150.0;
+}
+
+static void
+DeleteTri(GLint h)
+{
+  objects[h] = objects[objectCount - 1];
+  objectCount--;
+}
+
+static void
+GrowTri(GLint h)
+{
+  float v[2];
+  float *oldV;
+  GLint i;
+
+  v[0] = objects[h].v1[0] + objects[h].v2[0] + objects[h].v3[0];
+  v[1] = objects[h].v1[1] + objects[h].v2[1] + objects[h].v3[1];
+  v[0] /= 3;
+  v[1] /= 3;
+
+  for (i = 0; i < 3; i++) {
+    switch (i) {
+    case 0:
+      oldV = objects[h].v1;
+      break;
+    case 1:
+      oldV = objects[h].v2;
+      break;
+    case 2:
+      oldV = objects[h].v3;
+      break;
+    }
+    oldV[0] = 1.5 * (oldV[0] - v[0]) + v[0];
+    oldV[1] = 1.5 * (oldV[1] - v[1]) + v[1];
+  }
+}
+
+static void
+Mouse(int button, int state, int mouseX, int mouseY)
+{
+  GLint hit;
+
+  if (state == GLUT_DOWN) {
+    hit = DoSelect((GLint) mouseX, (GLint) mouseY);
+    if (hit != -1) {
+      if (button == GLUT_LEFT_BUTTON) {
+        RecolorTri(hit);
+      } else if (button == GLUT_MIDDLE_BUTTON) {
+        GrowTri(hit);
+      } else if (button == GLUT_RIGHT_BUTTON) {
+        DeleteTri(hit);
+      }
+      glutPostRedisplay();
+    }
+  }
+}
+
+static void
+Draw(void)
+{
+  glPushMatrix();
+  glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+  gluOrtho2D(-175, 175, -175, 175);
+  glMatrixMode(GL_MODELVIEW);
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+  glScalef(zoom, zoom, zoom);
+  glRotatef(zRotation, 0, 0, 1);
+  Render(GL_RENDER);
+  glPopMatrix();
+  glutSwapBuffers();
 }
 
-void mouse(int button, int state, int x, int y)
+static void
+DumpFeedbackVert(GLint * i, GLint n)
 {
-  switch (button) {
-  case GLUT_LEFT_BUTTON:
-    if (state == GLUT_DOWN) {
-      /* アニメーション開始 */
-      glutIdleFunc(idle);
-    }
-    else {
-      /* アニメーション停止 */
-      glutIdleFunc(0);
-    }
-    break;
-  case GLUT_RIGHT_BUTTON:
-    if (state == GLUT_DOWN) {
-      /* コマ送り (1ステップだけ進める) */
-        glutPostRedisplay();
-    }
-    break;
-  default:
-    break;
+  GLint index;
+
+  index = *i;
+  if (index + 7 > n) {
+    *i = n;
+    printf("  ???\n");
+    return;
   }
+  printf("  (%g %g %g), color = (%4.2f %4.2f %4.2f)\n",
+    feedBuf[index],
+    feedBuf[index + 1],
+    feedBuf[index + 2],
+    feedBuf[index + 3],
+    feedBuf[index + 4],
+    feedBuf[index + 5]);
+  index += 7;
+  *i = index;
 }
-  
-void keyboard(unsigned char key, int x, int y)
+
+static void
+DrawFeedback(GLint n)
 {
+  GLint i;
+  GLint verts;
+
+  printf("Feedback results (%d floats):\n", n);
+  for (i = 0; i < n; i++) {
+    switch ((GLint) feedBuf[i]) {
+    case GL_POLYGON_TOKEN:
+      printf("Polygon");
+      i++;
+      if (i < n) {
+        verts = (GLint) feedBuf[i];
+        i++;
+        printf(": %d vertices", verts);
+      } else {
+        verts = 0;
+      }
+      printf("\n");
+      while (verts) {
+        DumpFeedbackVert(&i, n);
+        verts--;
+      }
+      i--;
+      break;
+    case GL_LINE_TOKEN:
+      printf("Line:\n");
+      i++;
+      DumpFeedbackVert(&i, n);
+      DumpFeedbackVert(&i, n);
+      i--;
+      break;
+    case GL_LINE_RESET_TOKEN:
+      printf("Line Reset:\n");
+      i++;
+      DumpFeedbackVert(&i, n);
+      DumpFeedbackVert(&i, n);
+      i--;
+      break;
+    default:
+      printf("%9.2f\n", feedBuf[i]);
+      break;
+    }
+  }
+  if (i == MAXFEED) {
+    printf("...\n");
+  }
+  printf("\n");
+}
+
+static void
+DoFeedback(void)
+{
+  GLint x;
+
+  glFeedbackBuffer(MAXFEED, GL_3D_COLOR, feedBuf);
+  (void) glRenderMode(GL_FEEDBACK);
+
+  glPushMatrix();
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluOrtho2D(-175, 175, -175, 175);
+  glMatrixMode(GL_MODELVIEW);
+
+  glClearColor(0.0, 0.0, 0.0, 0.0);
+  glClear(GL_COLOR_BUFFER_BIT);
+
+  glScalef(zoom, zoom, zoom);
+  glRotatef(zRotation, 0, 0, 1);
+
+  Render(GL_FEEDBACK);
+
+  glPopMatrix();
+
+  x = glRenderMode(GL_RENDER);
+  if (x == -1) {
+    x = MAXFEED;
+  }
+  DrawFeedback((GLint) x);
+}
+
+/* ARGSUSED1 */
+static void
+Key(unsigned char key, int x, int y)
+{
+
   switch (key) {
-  case 'q':
-  case 'Q':
-  case '\033':  /* '\033' は ESC の ASCII コード */
+  case 'z':
+    zoom /= 0.75;
+    glutPostRedisplay();
+    break;
+  case 'Z':
+    zoom *= 0.75;
+    glutPostRedisplay();
+    break;
+  case 'f':
+    DoFeedback();
+    glutPostRedisplay();
+    break;
+  case 'l':
+    linePoly = !linePoly;
+    if (linePoly) {
+      glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    } else {
+      glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+    }
+    glutPostRedisplay();
+    break;
+  case 27:
     exit(0);
-  default:
+  }
+}
+
+/* ARGSUSED1 */
+static void
+SpecialKey(int key, int x, int y)
+{
+
+  switch (key) {
+  case GLUT_KEY_LEFT:
+    zRotation += 0.5;
+    glutPostRedisplay();
+    break;
+  case GLUT_KEY_RIGHT:
+    zRotation -= 0.5;
+    glutPostRedisplay();
     break;
   }
 }
 
-void init(void)
-{
-  glClearColor(1.0, 1.0, 1.0, 1.0);
-
-  glEnable(GL_DEPTH_TEST);
-
-  glEnable(GL_CULL_FACE);
-  glCullFace(GL_FRONT);
-
-  glEnable(GL_LIGHTING);
-  glEnable(GL_LIGHT0);
-  glEnable(GL_LIGHT1);
-  glLightfv(GL_LIGHT1, GL_DIFFUSE, green);
-  glLightfv(GL_LIGHT1, GL_SPECULAR, green);
-}
-
-int main(int argc, char *argv[])
+int
+main(int argc, char **argv)
 {
   glutInit(&argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-  glutCreateWindow("test");
-  glutDisplayFunc(display);
-  glutReshapeFunc(resize);
-  glutMouseFunc(mouse);
-  glutKeyboardFunc(keyboard);
-  init();
+  glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+  glutCreateWindow("Select Test");
+  Init();
+  glutReshapeFunc(Reshape);
+  glutKeyboardFunc(Key);
+  glutSpecialFunc(SpecialKey);
+  glutMouseFunc(Mouse);
+  glutDisplayFunc(Draw);
   glutMainLoop();
-  return 0;
+  return 0;             /* ANSI C requires main to return int. */
 }
